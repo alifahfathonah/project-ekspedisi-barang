b6e0a05b629cf9e08ea80ede4a41a1bfab48974d
ulylabsor <ulylabsor@gmail.com>
2020-10-03 17:36:48 +0700

    menampilkan data users di halaman admin

---
+++ admin-cp/index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require '../koneksi/koneksi.php';
+require '../koneksi/function.php';
+ob_start();
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+   <?php include 'kepala.php'; ?>
+</head>
+
+<body>
+   <?php include 'header.php'; ?>
+   <div class="row">
+      <div class="col-md-3">
+         <?php include 'menu.php'; ?>
+      </div>
+
+      <div class="col-md-9">
+         <?php include 'content.php'; ?>
+      </div>
+   </div>
+
+   <?php include 'kaki.php'; ?>
+</body>
+
+</html>
