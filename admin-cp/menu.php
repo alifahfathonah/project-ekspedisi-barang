98616a6b83f8ae1a40ed30334fc7ae1d3f0dd928
ulylabsor <ulylabsor@gmail.com>
2020-10-15 21:22:51 +0700

    data barang

--- admin-cp/menu.php
+++ admin-cp/menu.php
@@ -6,7 +6,11 @@
 
    <a href="?page=users-2" class="list-group-item list-group-item-action <?= isset($_GET['page']) ? $_GET['page'] == 'users-2' ? 'active' : '' : '' ?>">Users-2</a>
 
-   <a href="?page=datalayanan" class="list-group-item list-group-item-action">Data Layanan</a>
-   <a href="#" class="list-group-item list-group-item-action">Barang</a>
+   <a href="?page=datalayanan" class="list-group-item list-group-item-action <?= isset($_GET['page']) ? $_GET['page'] == 'datalayanan' ? 'active' : '' : '' ?>">Data Layanan</a>
+
+
+   <a href="?page=barang" class="list-group-item list-group-item-action <?= isset($_GET['page']) ? $_GET['page'] == 'barang' ? 'active' : '' : '' ?>">Barang</a>
+
+
    <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Tracking</a>
 </div>
