40b92613d3695f39ce043ae072947480b4a076d5
ImamRAP <bluriv3r@gmail.com>
2020-10-16 20:09:49 +0700

    menambahkan sebagian isi add-barang

--- admin-cp/view/add-form/add-barang.php
+++ admin-cp/view/add-form/add-barang.php
@@ -1,1 +1,34 @@
 <h3>Tambah Data Barang</h3>
+<form action="?page=act-barang" method="POST">
+<div class="row">
+<div class="col">
+<div class="form-group">
+<label for="namabarang">Nama Barang</label>
+<input type="text" class="form-control" id="barang" name="barang">
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col">
+<div class="form-group">
+<label for="jenis">Jenis</label>
+<textarea name="form-control" id="jenis" name="jenis" rows="3"></textarea>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-lg-6">
+<div class="form-group">
+<label for="berat">Berat</label>
+<input type="text" class="form-control" id="berat" name="berat">
+</div>
+</div>
+<div class="col-lg-6">
+<div class="form-group">
+<label for="foto">Foto</label>
+
+</div>
+</div>
+</div>
